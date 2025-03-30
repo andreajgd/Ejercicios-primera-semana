a16fa46a291ab9bb35ed0715147b23cabe567a9b
andrea1234e <122126891+andrea1234e@users.noreply.github.com>
2025-03-30 14:40:25 -0600

    Ejercicios primera sesión

--- ejercicio3.py
+++ ejercicio3.py
@@ -3,14 +3,35 @@
 # sobre 3. Diseñe un programa que determine el monto de la compra, el monto del descuento, el monto a pagar y el número de unidades de obsequio 
 # por la compra de cierta cantidad de docenas del producto.
 
+import os 
+os.system('cls' if os.name == 'nt' else 'clear')
 
-op =0
+op =1
+docena = 1500
+
 while op == 1:
     print("Venta al por mayor. ")
     
-    cantidad= int(input("Escribe la cantidad de docenas a adquirir: "))
-    if(cantidad < 1):
+    cant= int(input("Escribe la cantidad de docenas a adquirir: "))
+    if(cant < 1):
         print("Por favor ingresa una cantidad valida.")
-    # if(cantidad <3):
-    #     desc = 
+        continue
+    elif(cant <3):
+        cantidad = cant * docena
+        desc = cantidad -  (0.1* cantidad)
+        print(f"El precio de las {cant} docenas es: {desc}")
+    elif (cant == 3):
+        cantidad = cant * docena
+        desc = cantidad - (0.15* cantidad)
+        print(f"El precio de las {cant} docenas es: {desc}")
+    elif (cant > 3):
+        cantidad = cant * docena
+        desc = cantidad - (0.15* cantidad)
+        obsequio = (cant-3)/3 
+        print(f"El precio de las {cant} docenas es: {desc}, y {obsequio} unidades de obsequio!")
+    
+    op = int(input("Desea comprar de nuevo? 1: sí, 2: salir: "))
+
+print("Saliste del programa. ")
+
  
--- ejercicio4.py
+++ ejercicio4.py
@@ -1,2 +1,33 @@
 #Diseñe un programa que lea un número de tres cifras y determine si es igual al revés del número.
 
+
+import os 
+os.system('cls' if os.name == 'nt' else 'clear')
+op = 1
+
+while op == 1:
+    print("+-"*50)
+    print("Este programa determina si el número es igual al revés.")
+    print("Escribe un número de 3 cifras. ")
+    print("+-"*50)
+    num = int(input("Escribe un número: "))
+
+    if 100 <= num <= 999:
+        unidad = num % 10         
+        decena = (num // 10) % 10  
+        centena = num // 100        
+        
+        num_invertido = (unidad * 100) + (decena * 10) + centena
+
+        if num == num_invertido:
+            print(f"{num} es igual al revés. \n{num} = {num_invertido}.")
+        else:
+            print(f"{num} no es igual al revés.\n{num} != {num_invertido}.")
+
+    else:
+        print("Debes ingresar un número de 3 cifras.")
+    
+
+    op = int(input("1= Ingresar otro número, 2= Salir."))
+
+print("Saliste del programa.")
